19816c08630ad4f0c89dab7f82da40f08039c387
Jordan Shayo <jordanhildebrand2012@gmail.com>
2024-06-03 05:28:39 +0100

    configure router and added lazy loading

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CoreModule,
-    ShopModule,
+    HomeModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
